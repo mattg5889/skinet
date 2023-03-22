d5ccbafad290689b97e4f3f54e77f2e10b99095f
mattg5889 <mattg5889@users.noreply.github.com>
2023-03-22 12:08:29 -0500

    Test 2 commit

--- API/Program.cs
+++ API/Program.cs
@@ -25,7 +25,7 @@ app.UseStatusCodePagesWithReExecute("/errors/{0}");
     app.UseSwagger();
     app.UseSwaggerUI();
 //}
-// test commit to git
+// test commit
 app.UseStaticFiles();
 
 app.UseAuthorization();
