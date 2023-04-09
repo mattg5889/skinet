215e6eb3621ff11c9dec60df33834b92163e511c
mattg5889 <mattg5889@users.noreply.github.com>
2023-04-09 15:34:19 -0500

    End of section 8.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: (response) => this.products = response.data, // What to do next.
+      error: error => console.log(error), // What to do with an error
+      complete: () => { // What to do on completion.
+        console.log('Request completed.');
+        console.log('Extra statement.');
+      }
+    })
+  }
 }
